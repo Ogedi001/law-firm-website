3ea7ec4dc6be4367c2e5ef5265215498f5cd416a
ogedi001 <ogedifavour2@gmail.com>
2023-10-31 20:00:22 +0100

    tiny adjustment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, "public", "home.html"));
+   res.sendFile(path.join(__dirname, "public", "/html/home.html"));
 })
 app.listen(3000, () => {
     console.log('app is live at port 3000');
