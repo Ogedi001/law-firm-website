3b4792a038ba305d26c125874b95e9d075a53a52
ogedi001 <ogedifavour2@gmail.com>
2023-10-31 19:57:35 +0100

    adjustment

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-console.log(express.static(path.join(__dirname, "html")));
 // Set the directory to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/home', (req, res) => {
-   res.sendFile(path.join(__dirname, "html", "home.html"));
+app.get('/', (req, res) => {
+   res.sendFile(path.join(__dirname, "public", "home.html"));
 })
 app.listen(3000, () => {
     console.log('app is live at port 3000');
